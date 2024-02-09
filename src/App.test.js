d087c1380fd57791c966ab8fbca2b0a615f65828
Pinkivikram <151770607+Pinkivikram@users.noreply.github.com>
2024-02-09 10:36:05 +0000

    Update App.test.js -GROSS SALARY CALCULATOR

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,6 @@ import App from './App';
 
 test('renders VAT Calculator text', () => {
   render(<App />);
-  const heading = screen.getByText(/INCOME TAX CALCULATOR/i);
+  const heading = screen.getByText(/GROSS SALARY CALCULATOR/i);
   expect(heading).toBeInTheDocument();
 });
